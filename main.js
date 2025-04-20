c6bed5a5193de548ed050fcd2b06f25575434610
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-04-20 18:18:24 -0400

    feature: Add function to display books in library

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        pages: 295,
+        read: true,
+        id: crypto.randomUUID(),
+        info: function() {
+            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+        }
+    }
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -15,3 +26,25 @@ function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
+
+const books = document.querySelector(".books");
+
+function displayBooks() {
+    books.innerHTML = "";
+    myLibrary.forEach( book => {
+        const bookItem = document.createElement("li");
+        bookItem.classList.add("book-item");
+        bookItem.innerHTML = `
+            <div class="book-card">
+                <h2>${book.title}</h2>
+                <p>Author: ${book.author}</p>
+                <p>Pages: ${book.pages}</p>
+                <p>Read: ${book.read ? "Yes" : "No"}</p>
+            </div>
+        `;
+            // <button class="btn-delete" data-id="${book.id}">Delete</button>
+        books.appendChild(bookItem);
+    });
+}
+
+displayBooks();
