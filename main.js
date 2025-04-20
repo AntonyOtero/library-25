6e69cf02b331953610796ed0c5e844fa2c48ef6c
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-04-20 17:59:10 -0400

    feature: Add library array to hold books, Book constructor, and a function to add to the library

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = crypto.randomUUID();
+  this.info = function() {
+    return `${title} by ${author}, ${pages} pages, ${read}`;
+  };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
